53651371bd9a6f9b91cb3aae7e6b9d10d508fe8b
NoLimits <floreslenguaeduardo@gmail.com>
2023-11-27 02:00:55 -0500

    base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LabsComponent } from './pages/labs/labs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', title: 'Home', component: HomeComponent},
+    {path: 'labs', title: 'Labs', component: LabsComponent}
+];
